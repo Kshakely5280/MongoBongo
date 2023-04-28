93a891f0b48a8ac3c90b6e02c4470a095cf4ac36
Kyle Shakely <kyleshakely@yahoo.com>
2023-04-27 20:39:54 -0600

    added user schema

---
+++ models/user.js
@@ -0,0 +1,39 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+..+/, "Please enter a valid email address"],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
+const User = model("User", userSchema);
+
+module.exports = User;
