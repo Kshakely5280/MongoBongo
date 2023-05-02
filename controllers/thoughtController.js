ba345c3b6765827ff7ddbd832083bcdb6f3091aa
Kyle Shakely <kyleshakely@yahoo.com>
2023-05-02 15:23:15 -0600

    psuedo code out controllers

---
+++ controllers/thoughtController.js
@@ -0,0 +1,11 @@
+// const { User, Thought } = require('../models');
+
+module.exports = {
+    // getThought,
+    // getSingleThought,
+    // createThought,
+    // updateThought,
+    // deleteThought,
+    // createReaction,
+    // deleteReaction
+}
---
+++ controllers/userController.js
@@ -0,0 +1,12 @@
+// const { ObjectId } = require('mongoose').Types;
+// const { Student, Course } = require('../models');
+
+module.exports = {
+    // getUser,
+    // getSingleUser,
+    // createUser,
+    // updateUser,
+    // deleteUser,
+    // addFriend,
+    // deleteFriend
+}
